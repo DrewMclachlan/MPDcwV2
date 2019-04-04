dc3b747ed4ab9ba002f3cff54aeda8ef91942eb4
Drew <uni@Drew.local>
2019-04-04 17:44:26 +0100

    implemented port & land views

--- app/src/main/java/gcu/mpd/myapp/MainActivity.java
+++ app/src/main/java/gcu/mpd/myapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
